f930c30ae0d5cac8aa4e2ee6cfd522956727d15f
raviolliiii <90448227+raviolliiii@users.noreply.github.com>
2025-12-13 19:45:05 +0100

    Menu

---
+++ dzemCiastkowy/Source/Game/Scripts/Menu/PlayGame.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using FlaxEngine;
+using FlaxEngine.GUI;
+
+namespace Game;
+
+public class PlayGame : Script
+{
+    public SceneReference gameScene;
+    private bool isLoading = false;
+
+    public override void OnUpdate()
+    {
+        if (isLoading) return;
+
+        var uiControl = Actor as UIControl;
+        if (uiControl != null && uiControl.Get<Button>().IsPressed)
+        {
+            if (gameScene == null) return;
+
+            isLoading = true;
+
+            Scripting.RunOnUpdate(() =>
+            {
+                Level.UnloadAllScenes();
+                Level.LoadScene(gameScene);
+            });
+        }
+    }
+}
