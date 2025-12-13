2c11442c45c264661d0f836110c53842a91472f9
raviolliiii <90448227+raviolliiii@users.noreply.github.com>
2025-12-13 01:05:00 +0100

    Added NPC system
    - new class NPC_Class
    - new actor NPC_ACTOR - NPC management system and UI

---
+++ dzemCiastkowy/Source/Game/Scripts/NPC_System.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+using FlaxEngine.GUI;
+
+
+
+/// <summary>
+/// NPC_System Script.
+/// </summary>
+namespace Game.NPC
+{
+    public class NPC_System : Script
+    {
+        public UIControl[] NPC_UIs = new UIControl[3];
+        public NPC_Class[] NPCs = new NPC_Class[3];
+        /// <inheritdoc/>
+        public override void OnStart()
+        {
+            
+            // Here you can add code that needs to be called when script is created, just before the first game update
+        }
+
+        /// <inheritdoc/>
+        public override void OnEnable()
+        {
+            // Here you can add code that needs to be called when script is enabled (eg. register for events)
+        }
+
+        /// <inheritdoc/>
+        public override void OnDisable()
+        {
+            // Here you can add code that needs to be called when script is disabled (eg. unregister from events)
+        }
+
+        /// <inheritdoc/>
+        public override void OnUpdate()
+        {
+            // Here you can add code that needs to be called every frame
+        }
+    }
+
+};
