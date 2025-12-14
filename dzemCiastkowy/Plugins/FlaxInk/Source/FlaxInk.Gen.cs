3040dad7d9551424ee24f3d1a1b33e8ab8a8bea5
Symarfiusz-Renesans <patalitaszymon@gmail.com>
2025-12-14 12:02:21 +0100

    Dodanie kolejnego obrazka

---
+++ dzemCiastkowy/Plugins/FlaxInk/Source/FlaxInk.Gen.cs
@@ -0,0 +1,19 @@
+// This code was auto-generated. Do not modify it.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("FlaxInk")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tryibion")]
+[assembly: AssemblyProduct("FlaxEngine")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("1337b992-b992-1337-55fd-9f6a05150000")]
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0")]
+[assembly: DisableRuntimeMarshalling]
